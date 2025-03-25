bde6783e0205fc19f642f30f01ac11547b8a10d9
chrisreis53 <chrisreis53@gmail.com>
2025-03-25 11:27:10 -0700

    Basic Schedule Builder Javascript File

---
+++ inc/scheduler.js
@@ -0,0 +1,44 @@
+function build_schedule(container_id, groundstations, pass_data, options, ) {
+
+    // DOM element where the Timeline will be attached
+    var container = document.getElementById(container_id);
+
+    function findObjectById(array, name) {
+        const found = array.find(obj => obj.content === name);
+        return found.id;
+    }
+
+    var i = 0;
+    stations = [];
+    groundstations.forEach(station => {
+        stations.push({
+            id: i,
+            content:station.abbreviation
+        });
+        i += 1;
+    });
+    //Create Group
+    var group = new vis.DataSet(stations);
+
+    //Add Passes
+    var j = 0;
+    passes = [];
+    pass_data.forEach(pass => {
+        passes.push({
+            id: j,
+            editable: false,
+            content:"Max El: ",
+            start:new Date(pass.start),
+            end:new Date(pass.end),
+            group:findObjectById(stations,pass.site)
+        });
+        j += 1;
+    });
+    var items = new vis.DataSet(passes);
+
+    console.log(passes);
+
+    // Create a Timeline
+    var timeline = new vis.Timeline(container_id, items, group, options);
+    
+}
